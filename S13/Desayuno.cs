d1bf7a63fe099f7a42cdb074f0701b2402b69b00
Marl0023 <marlotay2312@gmail.com>
2024-11-07 09:46:58 -0500

    FC1

---
+++ S13/Desayuno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S13
+{
+    public class Desayuno
+    {
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public string Dias { get; set; }
+
+        public override string? ToString()
+        {
+            return $"Desayuno {Nombre} s/{Precio}. Servidos los {Dias}.";
+        }
+        public string[] ListaDias() { 
+            return Dias.Split(',');
+        }
+    }
+    
+    
+}
---
+++ S13/Program.cs
@@ -0,0 +1,15 @@
+﻿using S13;
+Desayuno d1 = new Desayuno();
+d1.Nombre = "Chaufa especial";
+d1.Precio = 10.5;
+d1.Dias = "Lun,Mar,Mie";
+Console.WriteLine(d1);
+string[] lsita = d1.ListaDias();
+foreach (string str in lsita) { 
+    Console.WriteLine(str);
+}
+
+Desayuno d2 = new Desayuno();
+d2.Nombre = "Sopa de casa";
+Desayuno d3 = new Desayuno();
+d3.Nombre = "Queque de choclo";
