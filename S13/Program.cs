852dcef1f017882b1875666f3f976f35b13c5bb7
SebastianGarb <sebastiangarboza63@hotmnai.com>
2024-11-13 18:38:01 -0500

    Le agregue la estructura

--- S13/Program.cs
+++ S13/Program.cs
@@ -1,4 +1,5 @@
-﻿using S13;
+﻿using S13; 
+/*
 Desayuno d1 = new Desayuno();
 d1.Nombre = "Chaufa especial";
 d1.Precio = 10.5;
@@ -13,3 +14,13 @@ Desayuno d2 = new Desayuno();
 d2.Nombre = "Sopa de casa";
 Desayuno d3 = new Desayuno();
 d3.Nombre = "Queque de choclo";
+*/
+
+Desayuno d1 = new Desayuno();
+bool u = true;
+
+do
+{
+
+}while (u == true);
+
