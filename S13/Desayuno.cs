41f5537c24f37bb2e5c4a319be9d2b643728f530
SebastianGarb <sebastiangarboza63@hotmnai.com>
2024-11-13 18:40:11 -0500

    Le agregue el metodo eliminar

--- S13/Desayuno.cs
+++ S13/Desayuno.cs
@@ -19,9 +19,30 @@ namespace S13
         public string[] ListaDias() { 
             return Dias.Split(',');
         }
-        private string[] menu = new string[] { "chaufa", "Pan con pollo", "Pan con lomo" }; 
+        private string[] menu = new string[] { "chaufa", "Pan con pollo", "Pan con lomo" };
 
-
+        public void Eliminar(string emenu)
+        {
+            int index = Array.IndexOf(menu, emenu);//2
+            if (index == -1)
+            {
+                Console.WriteLine("Nombre no encontrado");
+                return;
+            }
+            string[] nuevomenu2 = new string[menu.Length - 1];
+            for (int i = 0, j = 0; i < menu.Length; i++)
+            {
+                if (i != index)
+                {
+                    nuevomenu2[j] = menu[i];
+                    j++;
+                }
+            }
+            menu = nuevomenu2;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Menu {emenu} Elimando Correctamente");
+            Console.ResetColor();
+        }
     }
     
     
